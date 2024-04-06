ff8461fe4a29a35b8f138100a324311f477d7a52
gihyeonchoi <162656640+gihyeonchoi@users.noreply.github.com>
2024-04-06 14:20:03 +0900

    Update 240405_로또랜덤생성.py

--- 240405_로또랜덤생성.py
+++ 240405_로또랜덤생성.py
@@ -2,13 +2,19 @@ import random
 
 userlotto = []
 for i in range(5):
-    inputlotto = input("1~10 까지의 숫자를 골라주세요 : ")
-    userlotto.append(int(inputlotto))
+    while True:
+        inputlotto = input("1~10 까지의 숫자를 골라주세요 : ")
+        inputlotto = int(inputlotto)
+        if inputlotto in userlotto:
+            print("이미 선택한 숫자입니다. 다른 숫자를 선택해주세요.")
+        else:
+            userlotto.append(inputlotto)
+            break
 print(userlotto)
 
 randlotto = []
 for i in range(5):
-    randlotto.append(random.randint(1,10))
+    randlotto = random.sample(range(1, 10), 5)
 print(randlotto)
 
 rank = 0
@@ -30,6 +36,3 @@ elif rank == 1:
     print("1개 맞추셨습니다")
 else:
     print("0개 맞추셨습니다")
-
-#if lotto[0] == lottowin[0]:
-    
